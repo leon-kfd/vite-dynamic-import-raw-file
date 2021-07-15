fc9fb414342016d28bd45c94d41a4d3cfac82be6
leon-kfd <kfd_personal@163.com>
2021-07-15 11:09:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+## BUG REPORT TO VITE
+
+Question: Can not dynamically import a raw file in production mode.
+
+Reproduction:
+
++ yarn
++ npm run build
++ npm run serve
+
+> Click load file button, will throw error in console.
+
+![image](https://user-images.githubusercontent.com/30256102/125721157-15cd0249-8dfc-4d09-bd0f-aad694c00475.png)
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  Hello.
+  <br>
+  <button @click="load(1)">Load File1</button>
+  <button @click="load(2)">Load File2</button>
+  <pre>{{current}}</pre>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const current = ref('')
+    return {
+      current,
+      async load(index) {
+        const file = await import(`./source/file${index}.md?raw`)
+        current.value = file.default
+        console.log('file', file)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
